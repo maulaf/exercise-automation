ce2c08025b7ae50dc27df1d45410fba40f97185c
maulaf <fetty.maula98@gmail.com>
2026-01-02 16:59:26 +0700

    add TC9 and TC10

---
+++ Scripts/Test Case 11 - Verify Subscription in Cart page/Script1767347804274.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.click(findTestObject('Object Repository/href', [('href') : '/view_cart']))
+
+WebUI.waitForPageLoad(0)
+
+WebUI.scrollToElement(findTestObject('Object Repository/id', [('id') : 'footer']), 0)
+
+WebUI.verifyTextPresent("SUBSCRIPTION", false)
+
+email = 'fetty@mail.com'
+
+WebUI.setText(findTestObject('Object Repository/id', [('id') : 'susbscribe_email']), email)
+
+WebUI.click(findTestObject('Object Repository/id', [('id') : 'subscribe']))
+
+WebUI.verifyTextPresent("You have been successfully subscribed!", false)
